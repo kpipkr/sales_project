732f11f5d306273484db4d26905ec3a0d9ab5fbd
Dicha Putra Arwindra <arwindra23@gmail.com>
2021-01-25 14:57:57 +0700

    Create sales_project.sql
    tambah struktur database sales

---
+++ sales_project.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Jan 2021 pada 08.57
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 7.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sales_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(100) NOT NULL,
+  `nama_perusahaan` varchar(500) NOT NULL,
+  `alamat` varchar(500) NOT NULL,
+  `contact_no_perusahaan` int(50) NOT NULL,
+  `nama_pic` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact_no_pic` int(50) NOT NULL,
+  `twitter` varchar(100) NOT NULL,
+  `fb` varchar(100) NOT NULL,
+  `wa` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kunjungan`
+--
+
+CREATE TABLE `kunjungan` (
+  `kunjungan_id` int(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `customer_id` int(100) NOT NULL,
+  `waktu_kunjungan` date NOT NULL,
+  `nama_pic` varchar(100) NOT NULL,
+  `contact_no_pic` int(50) NOT NULL,
+  `produk` varchar(100) NOT NULL,
+  `harga` int(255) NOT NULL,
+  `waktu_pembelian` date NOT NULL,
+  `lainlain` varchar(1000) NOT NULL,
+  `foto` varchar(500) NOT NULL,
+  `alasan` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `target`
+--
+
+CREATE TABLE `target` (
+  `target` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tipe` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indeks untuk tabel `kunjungan`
+--
+ALTER TABLE `kunjungan`
+  ADD PRIMARY KEY (`kunjungan_id`),
+  ADD KEY `user_ID` (`user_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kunjungan`
+--
+ALTER TABLE `kunjungan`
+  MODIFY `kunjungan_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `kunjungan`
+--
+ALTER TABLE `kunjungan`
+  ADD CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
