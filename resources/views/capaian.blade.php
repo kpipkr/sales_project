07b629d8204cbde2409c3efbae158d4f919a011b
Evi Oktavia K <evioktawati@gmail.com>
2021-01-25 08:54:27 +0700

    Update capaian.blade.php

--- resources/views/capaian.blade.php
+++ resources/views/capaian.blade.php
@@ -58,26 +58,26 @@
             <div class="container">
               <ul class="nav page-navigation">
                 <li class="nav-item">
-                  <a class="nav-link" href="index.html">
+                  <a class="nav-link" href="index">
                     <i class="mdi mdi-target menu-icon"></i>
                     <span class="menu-title">Target Kunjungan</span>
                   </a>
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="#">
+                  <a class="nav-link" href="capaian">
                     <i class="mdi mdi-chart-bar menu-icon"></i>
                     <span class="menu-title"><b>Capaian</b></span>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">
+                  <a class="nav-link" href="laporan">
                     <i class="mdi mdi-table-large menu-icon"></i>
                     <span class="menu-title">Laporan</span>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">
+                  <a class="nav-link" href="customer">
                     <i class="mdi mdi-menu menu-icon"></i>
                     <span class="menu-title">Customer</span>
                   </a>
@@ -96,103 +96,116 @@
           <div class="main-panel">
             <div class="content-wrapper pb-0">
               <!-- table row starts here -->
-             <!-- doughnut chart row starts -->
-             <div class="card">
-              <div class="card-body">
-                <h4 class="card-title">Nama Karyawan : </h4>
-              </p>
-              <div class="table-responsive">
-                <table class="table table-striped">
-                  <thead>
-                    <tr>
-                      <th><center>No.</center></th>
-                      <th><center>Nama Perusahaan</center></th>
-                      <th><center>Catatan</center></th>
-                      <th><center>Action</center></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <center>1</center>
-                      </td>
-                      <td>PT. Maju Bersama</td>
-                      <td>
-                        <div class="catatan">
-                          -
-                        </div>
-                      </td>
-                      <td><center><button type="button" class="btn btn-secondary">Detail</button></center></button></td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <center>2</center>
-                      </td>
-                      <td></td>
-                      <td>
-                        <div class="catatan">
-                          Tidak bisa kunjungan karena ...
-                        </div>
-                      </td>
-                      <td>
-                        <center>
-                          <button type="button" class="btn btn-success"><a><span class="mdi mdi-check"></span></a></button>
-                          <button type="button" class="btn btn-danger"><a><span class="mdi mdi-close"></span></a></button>
-                        </center>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+              <!-- doughnut chart row starts -->
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Nama Karyawan</h4>
+                    <select class="js-example-basic-single">
+                      <option value="namakaryawan">A</option>
+                      <option value="namakaryawan">B</option>
+                      <option value="namakaryawan">C</option>
+                      <option value="namakaryawan">D</option>
+                    </select>
+                  <br>
+                  <div class="table-responsive">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th><center>No.</center></th>
+                          <th><center>Nama Perusahaan</center></th>
+                          <th><center>Catatan</center></th>
+                          <th><center>Action</center></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            <center>1</center>
+                          </td>
+                          <td>PT. Maju Bersama</td>
+                          <td>
+                            <div class="catatan">
+                              -
+                            </div>
+                          </td>
+                          <td><center><button type="button" class="btn btn-secondary">Detail</button></center></button></td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <center>2</center>
+                          </td>
+                          <td></td>
+                          <td>
+                            <div class="catatan">
+                              Tidak bisa kunjungan karena ...
+                            </div>
+                          </td>
+                          <td>
+                            <center>
+                              <button type="button" class="btn btn-success"><a><span class="mdi mdi-check"></span></a></button>
+                              <button type="button" class="btn btn-danger"><a><span class="mdi mdi-close"></span></a></button>
+                            </center>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <!-- last row starts here -->
-        <div class="row">
-          <div class="col-sm-6 col-xl-4 stretch-card grid-margin">
-            <div class="card">
+            <!-- last row starts here -->
+            <div class="row">
+              <div class="col-sm-6 col-xl-4 stretch-card grid-margin">
+                <div class="card">
 
+                </div>
+              </div>
             </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            <footer class="footer">
+              <div class="container">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                  <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © KPI Pakerin 2021</span>
+                  <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.pakerin.co.id/" target="_blank">PT. Pakerin</a></span>
+                </div>
+              </div>
+            </footer>
+            <!-- partial -->
           </div>
+          <!-- main-panel ends -->
         </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="container">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © KPI Pakerin 2021</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.pakerin.co.id/" target="_blank">PT. Pakerin</a></span>
-            </div>
-          </div>
-        </footer>
-        <!-- partial -->
+        <!-- page-body-wrapper ends -->
       </div>
-      <!-- main-panel ends -->
-    </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
-  <script src="../assets/vendors/chart.js/Chart.min.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="../assets/js/off-canvas.js"></script>
-  <script src="../assets/js/hoverable-collapse.js"></script>
-  <script src="../assets/js/misc.js"></script>
-  <script src="../assets/js/settings.js"></script>
-  <script src="../assets/js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page -->
-  <script src="../assets/js/dashboard.js"></script>
-  <!-- End custom js for this page -->
-</body>
-</html>
+      <!-- container-scroller -->
+      <!-- plugins:js -->
+      <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+      <!-- endinject -->
+      <script src="../../../assets/vendors/select2/select2.min.js"></script>
+      <script src="../../../assets/js/select2.js"></script>
+      <script src="../../../assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+      <!-- Plugin js for this page -->
+    <script src="../../../assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+      <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+      <script src="../assets/vendors/chart.js/Chart.min.js"></script>
+      <script src="../assets/vendors/flot/jquery.flot.js"></script>
+      <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
+      <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
+      <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
+      <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
+      <!-- End plugin js for this page -->
+      <!-- inject:js -->
+      <script src="../assets/js/off-canvas.js"></script>
+      <script src="../assets/js/hoverable-collapse.js"></script>
+      <script src="../assets/js/misc.js"></script>
+      <script src="../assets/js/settings.js"></script>
+      <script src="../assets/js/todolist.js"></script>
+      <!-- endinject -->
+      <!-- Custom js for this page -->
+      <script src="../assets/js/dashboard.js"></script>
+      <script src="../../../assets/js/file-upload.js"></script>
+    <script src="../../../assets/js/typeahead.js"></script>
+    <script src="../../../assets/js/select2.js"></script>
+      <!-- End custom js for this page -->
+    </body>
+    </html>
