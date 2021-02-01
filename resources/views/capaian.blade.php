2ad3114c2088bfec8e59ea3a28365fe520fc2d62
Evi Oktavia K <evioktawati@gmail.com>
2021-02-01 08:59:28 +0700

    capaian update

--- resources/views/capaian.blade.php
+++ resources/views/capaian.blade.php
@@ -121,24 +121,33 @@
                       </td>
                       <td><center><button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalSaya">Detail</button></center></td>
                       <!-- Modal Pop-Up -->
-                        <div class="modal fade" id="modalSaya" tabindex="-1" role="dialog" aria-labelledby="modalSayaLabel" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="modalSayaLabel">Perusahaan X</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
+                      <div class="modal fade" id="modalSaya" tabindex="-1" role="dialog" aria-labelledby="modalSayaLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="modalSayaLabel">Perusahaan X</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <form>
+                                <div class="form-group">
+                                  <label for="exampleInputEmail1">Email address</label>
+                                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                  <label for="exampleInputEmail1">Username</label>
+                                  <input type="text" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username">
+                                </div>
+                                <div class="form-group">
+                                  <label for="exampleInputPassword1">Password</label>
+                                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                </div>
+                              </form>
+                            </div>
+                              <div class="modal-footer">
                               </div>
-                              <div class="modal-body">
-                                Nama Perusahaan
-                                <br/>
-                                Seri Tutorial Bootstrap 4 lengkap dari dasar sampai mahir.
-                              </div>
-                              <!-- <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                                <button type="button" class="btn btn-primary">Oke</button>
-                              </div> -->
                             </div>
                           </div>
                         </div>
