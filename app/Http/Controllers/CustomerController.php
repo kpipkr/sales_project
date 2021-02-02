49daa4e1d82376669bc77d1dfc2c21ad8c0ff481
Dicha Putra Arwindra <arwindra23@gmail.com>
2021-02-02 11:34:00 +0700

    update view customer
    view list customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\customer;
 
 class CustomerController extends Controller {
 
     public function index() {
-        echo "customer controller";
+        //echo "customer controller";
+        $db = customer::select('nama_perusahaan','alamat','contact_no_perusahaan')->get();
 
         // Var pass to View
         $data = array(
-            'target' => 'la la la',
+            'target' => $db,
             'Description' => 'This is New Application',
             'author' => 'foo'
         );
