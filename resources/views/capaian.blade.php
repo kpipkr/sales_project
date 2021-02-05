fdc535a58577bf5c55122dbfe4cc9576b1f55ad3
Evi Oktavia K <evioktawati@gmail.com>
2021-02-05 08:23:44 +0700

    tes

--- resources/views/capaian.blade.php
+++ resources/views/capaian.blade.php
@@ -4,6 +4,45 @@
   <!-- Required meta tags -->
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <style type="text/css">
+    /* Table */
+    @media screen and (max-width: 520px) {
+      table {
+        width: 100%;
+      }
+      thead th.column-primary {
+        width: 100%;
+      }
+
+      thead th:not(.column-primary) {
+        display:none;
+      }
+
+      th[scope="row"] {
+        vertical-align: top;
+      }
+
+      td {
+        display: block;
+        width: auto;
+        text-align: right;
+      }
+      thead th::before {
+        text-transform: uppercase;
+        font-weight: bold;
+        content: attr(data-header);
+      }
+      thead th:first-child span {
+        display: none;
+      }
+      td::before {
+        float: left;
+        text-transform: uppercase;
+        font-weight: bold;
+        content: attr(data-header);
+      }
+    }
+  </style>
   <title>Sales Project</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
@@ -16,6 +55,14 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
   <!-- End plugin css for this page -->
+  <link rel="stylesheet" href="https://jagowebdev.com/wp-content/demo/includes/assets/css/site.css?r=1612421743"/>
+  <link rel="stylesheet" href="https://jagowebdev.com/wp-content/demo/includes/assets/css/icomoon/style.css?r=1612421743"/>
+  <link rel="stylesheet" href="https://jagowebdev.com/wp-content/demo/includes/assets/fonts/font-awesome/fontawesome-all.min.css?r=1612421743"/>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="https://jagowebdev.com/wp-content/demo/membuat-tabel-responsive-dengan-css/font-awesome/css/font-awesome.min.css?r=1612421743"/>
+  <link rel="stylesheet" href="https://jagowebdev.com/wp-content/demo/membuat-tabel-responsive-dengan-css/css/main.css?r=1612421743"/><script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=P41oijxkfFScRkDH3sZOq6PCJIyx-bBNQV5jsRespHKxIPa_remlMKVb6dvpAnOxJj-spdH_AsZpfbCBZ3MeSe-MZrmSj9dbDuCopgtR1SBevXg-ZuGmS09QJ4Yj_c9K" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9qYWdvd2ViZGV2LmNvbS93cC1jb250ZW50L2RlbW8vbWVtYnVhdC10YWJlbC1yZXNwb25zaXZlLWRlbmdhbi1jc3Mv"/><script type="text/javascript" src="https://jagowebdev.com/wp-content/demo/includes/assets/js/jquery-3.3.1.min.js"></script><script type="text/javascript" src="https://jagowebdev.com/wp-content/demo/includes/assets/js/html5.js"></script>
+  <script type="text/javascript" src="https://jagowebdev.com/wp-content/demo/membuat-tabel-responsive-dengan-css/js/main.js?r=1612421743"></script>
+  <link rel="shortcut icon" href="https://jagowebdev.com/wp-content/demo/includes/assets/img/favicon.png?hash=1612421743"/>
   <!-- inject:css -->
   <!-- endinject -->
   <!-- Layout styles -->
@@ -48,7 +95,6 @@
                 <span class="menu-title">Target Kunjungan</span>
               </a>
             </li>
-
             <li class="nav-item">
               <a class="nav-link" href="capaian">
                 <i class="mdi mdi-chart-bar menu-icon"></i>
@@ -101,147 +147,153 @@
               <div class="table-responsive">
                 <table class="table table-striped">
                   <thead>
+                    
                     <tr>
-                      <th><center>No.</center></th>
-                      <th><center>Nama Perusahaan</center></th>
-                      <th><center>Catatan</center></th>
-                      <th><center>Action</center></th>
+                      <th scope="col" class="column-primary">Nama Perusahaan</th>
+                      <th scope="col">Catatan</th>
+                      <th scope="col" class="column-primary">Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>
-                        <center>1</center>
-                      </td>
-                      <td>PT. Maju Bersama</td>
-                      <td>
-                        <div class="catatan">
-                          -
-                        </div>
-                      </td>
+                    <tr class="expanded">
+                      <td data-header="Nama Perusahaan" class="title">PT. Maju Bersama<a href="#" class="more"><i class="fa fa-chevron-up fa-chevron-down">::before</i></a></td>
+                      <td data-header="Catatan" class="title"></td>
+                      <th scope="row">tes
+                        <div>
+                          <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalSaya">Detail</button>
+                        </div> 
+                      </th>
                     </tr>
-                      <td><center><button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalSaya">Detail</button></center></td>
-                      <!-- Modal Pop-Up -->
-                      <div class="modal fade" id="modalSaya" tabindex="-1" role="dialog" aria-labelledby="modalSayaLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id="modalSayaLabel">Perusahaan X</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <form>
-                                <div class="form-group">
-                                  <label for="nama_pic">Nama PIC yang ditemui</label>
-                                  <input type="text" class="form-control" id="nama_pic" readonly>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Kontak PIC yang ditemui</label>
-                                  <input type="text" class="form-control" id="contact_no_pic" readonly>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Produk yang akan dibeli</label>
-                                  <textarea class="form-control" id="produk" readonly></textarea>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Kisaran Harga</label>
-                                  <input type="text" class="form-control" id="harga" readonly>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Perkiraan Waktu Pembelian</label>
-                                  <input type="text" class="form-control" id="waktu_pembelian" readonly>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Lain - Lain</label>
-                                  <textarea type="text" class="form-control" id="lainlain" readonly></textarea>
-                                </div>
-                                <div class="form-group">
-                                  <label for="nama_pic">Foto saat kunjungan</label>
-                                  <img id="foto" src="../assets/images/salesproject.jpg" alt="image" />
-                                  <input type="text" class="form-control" id="nama_pic" readonly>
-                                </div>
-                              </form>
-                            </div>
-                              <div class="modal-footer">
+                    <!-- Modal Pop-Up -->
+                    <div class="modal fade" id="modalSaya" tabindex="-1" role="dialog" aria-labelledby="modalSayaLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="modalSayaLabel">Perusahaan X</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            <form>
+                              <div class="form-group">
+                                <label for="nama_pic">Nama PIC yang ditemui</label>
+                                <input type="text" class="form-control" id="nama_pic" readonly>
                               </div>
-                            </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Kontak PIC yang ditemui</label>
+                                <input type="text" class="form-control" id="contact_no_pic" readonly>
+                              </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Produk yang akan dibeli</label>
+                                <textarea class="form-control" id="produk" readonly></textarea>
+                              </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Kisaran Harga</label>
+                                <input type="text" class="form-control" id="harga" readonly>
+                              </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Perkiraan Waktu Pembelian</label>
+                                <input type="text" class="form-control" id="waktu_pembelian" readonly>
+                              </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Lain - Lain</label>
+                                <textarea type="text" class="form-control" id="lainlain" readonly></textarea>
+                              </div>
+                              <div class="form-group">
+                                <label for="nama_pic">Foto saat kunjungan</label>
+                                <img id="foto" src="../assets/images/salesproject.jpg" alt="image" />
+                                <input type="text" class="form-control" id="nama_pic" readonly>
+                              </div>
+                            </form>
+                          </div>
+                          <div class="modal-footer">
                           </div>
                         </div>
-                      </td>
-                    <tr>
-                      <td>
-                        <center>2</center>
-                      </td>
-                      <td></td>
-                      <td>
-                        <div class="catatan">
-                          Tidak bisa kunjungan karena ...
-                        </div>
-                      </td>
-                      <td>
-                        <center>
-                          <button type="button" class="btn btn-success"><a><span class="mdi mdi-check"></span></a></button>
-                          <button type="button" class="btn btn-danger"><a><span class="mdi mdi-close"></span></a></button>
-                        </center>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+                      </div>
+                    </div>
+                  </td>
+                  <tr>
+                    <td>
+                      <center>
+                        <button type="button" class="btn btn-success"><a><span class="mdi mdi-check"></span></a></button>
+                        <button type="button" class="btn btn-danger"><a><span class="mdi mdi-close"></span></a></button>
+                      </center>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        <!-- last row starts here -->
-        <div class="row">
-          <div class="col-sm-6 col-xl-4 stretch-card grid-margin">
-          </div>
-        </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="container">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © KPI Pakerin 2021</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.pakerin.co.id/" target="_blank">PT. Pakerin</a></span>
-            </div>
-          </div>
-        </footer>
-        <!-- partial -->
       </div>
-      <!-- main-panel ends -->
+      <!-- last row starts here -->
+      <div class="row">
+        <div class="col-sm-6 col-xl-4 stretch-card grid-margin">
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+      <!-- partial:partials/_footer.html -->
+      <footer class="footer">
+        <div class="container">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © KPI Pakerin 2021</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.pakerin.co.id/" target="_blank">PT. Pakerin</a></span>
+          </div>
+        </div>
+      </footer>
+      <!-- partial -->
     </div>
-    <!-- page-body-wrapper ends -->
+    <!-- main-panel ends -->
   </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="../../../assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
-  <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
-  <script src="../assets/vendors/chart.js/Chart.min.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
-  <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="../assets/js/off-canvas.js"></script>
-  <script src="../assets/js/hoverable-collapse.js"></script>
-  <script src="../assets/js/misc.js"></script>
-  <script src="../assets/js/settings.js"></script>
-  <script src="../assets/js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
-  <!-- Custom js for this page -->
-  <script src="../assets/js/dashboard.js"></script>
-  <script src="../../../assets/js/file-upload.js"></script>
-  <script src="../../../assets/js/typeahead.js"></script>
-  <!-- End custom js for this page -->
+  <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('a.more').click(function() {
+
+    // Toggle Class
+    $tr = $(this).parent().parent();
+    $tr.toggleClass('expanded');
+    
+    // Tampilkan - sembunyikan baris
+    $i = $(this).children('i');
+    $i.removeClass('fa-chevron-down', 'fa-chevron-up');
+    var arrow = $tr.hasClass('expanded') ? 'fa-chevron-up' : 'fa-chevron-down';
+    $i.addClass(arrow);
+    
+    return false;
+  });
+  })
+</script>
+<!-- plugins:js -->
+<script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="../../../assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+<script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+<script src="../assets/vendors/chart.js/Chart.min.js"></script>
+<script src="../assets/vendors/flot/jquery.flot.js"></script>
+<script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
+<script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
+<script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
+<script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="../assets/js/off-canvas.js"></script>
+<script src="../assets/js/hoverable-collapse.js"></script>
+<script src="../assets/js/misc.js"></script>
+<script src="../assets/js/settings.js"></script>
+<script src="../assets/js/todolist.js"></script>
+<!-- endinject -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+<!-- Custom js for this page -->
+<script src="../assets/js/dashboard.js"></script>
+<script src="../../../assets/js/file-upload.js"></script>
+<script src="../../../assets/js/typeahead.js"></script>
+<!-- End custom js for this page -->
 </body>
 </html>
